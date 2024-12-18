4033a627a9e6db4332371f2c22a9a1db96b67c3a
Shashank Kumar <2021kucp1137@iiitkota.ac.in>
2024-12-18 16:32:22 +0530

    Hii some changes in backend

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./socket/socket.js";
-import path from "path";
 import dotenv from "dotenv";
  
 dotenv.config();
@@ -14,7 +13,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
 
 //middlewares
 app.use(express.json());
@@ -33,10 +31,12 @@ app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-})
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
 
 
 server.listen(PORT, () => {
