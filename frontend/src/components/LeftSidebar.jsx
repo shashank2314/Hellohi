16d71dac2f9beace3302bbb274c509c57077f877
Shashank Kumar <2021kucp1137@iiitkota.ac.in>
2024-12-18 20:36:01 +0530

    logout bug resolve

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -22,8 +22,10 @@ const LeftSidebar = () => {
     const logoutHandler = async () => {
         try {
             const BASE_URL = import.meta.env.VITE_SERVER_URL;
+
             const res = await axios.get(`${BASE_URL}/api/v1/user/logout`, { withCredentials: true });
             if (res.data.success) {
+                sessionStorage.removeItem("accessToken");
                 dispatch(setAuthUser(null));
                 dispatch(setSelectedPost(null));
                 dispatch(setPosts([]));
